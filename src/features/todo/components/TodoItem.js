31fe3e9346974b8832b8f4e209aa2feaa7d09c5c
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-07-24 21:35:41 +0800

    feat, update task status with redux

--- src/features/todo/components/TodoItem.js
+++ src/features/todo/components/TodoItem.js
@@ -1,8 +1,14 @@
+import { useDispatch } from "react-redux";
+import { update } from "../reducers/todoSlice";
 export default function TodoItem(props) {
-    const { task } = props;
+    const dispatch = useDispatch();
+    const handleClickTaskName = () => {
+        dispatch(update({ id:props.task.id, completed: !props.task.completed }));
+    }
     return(
         <div className='todo-item'>
-            <div className='task-name'>{task.name}</div>
+            <div className={`task-name ${ props.task.completed ? 'done' : ''}`}
+                 onClick={handleClickTaskName}>{props.task.name}</div>
             <div className='remove-button'>x</div>
         </div>
     );
--- src/features/todo/reducers/todoSlice.js
+++ src/features/todo/reducers/todoSlice.js
@@ -4,19 +4,27 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState: {
         tasks: [
-            {  id: 'd11839f8-3de0-4e9f-8c51-e07dfb76223f', name: "Clean the room", status: "Completed" },
-            {  id: '03b80cf0-d22d-4d2b-bfa6-837b1c14ebd7', name: "Learn React", status: "Completed" },
-            {  id: '7ebd6f46-e110-4b2d-bc59-80a112e078ed', name: "Schedule a meeting", status: "To be done" },
-            {  id: 'dbfd58f1-6313-4af4-88e1-188655b40196', name: "Reply to emails", status: "To be done" },
-            {  id: 'ed2b3d96-ff31-4ff0-ae70-a23dfabc6d37', name: "Meet a friend", status: "To be done" },
+            {  id: 'd11839f8-3de0-4e9f-8c51-e07dfb76223f', name: "Clean the room", completed: true },
+            {  id: '03b80cf0-d22d-4d2b-bfa6-837b1c14ebd7', name: "Learn React", completed: true },
+            {  id: '7ebd6f46-e110-4b2d-bc59-80a112e078ed', name: "Schedule a meeting", completed: false },
+            {  id: 'dbfd58f1-6313-4af4-88e1-188655b40196', name: "Reply to emails", completed: false },
+            {  id: 'ed2b3d96-ff31-4ff0-ae70-a23dfabc6d37', name: "Meet a friend", completed: false },
         ]
     },
     reducers: {
         create: (state, action) => {
-            state.tasks = [...state.tasks, { id:uuidv4(), ...action.payload,  status: "To be done" }]
+            state.tasks = [...state.tasks, { id:uuidv4(), ...action.payload,  completed: false }]
+        },
+        update: (state, action) => {
+            state.tasks = state.tasks.map( task => {
+                if(task.id === action.payload.id) {
+                    return { id: task.id, name: task.name, completed: action.payload.completed }
+                }
+                return task;
+            })
         }
     }
 })
 
-export const { create } = todoSlice.actions
+export const { create, update } = todoSlice.actions
 export default todoSlice.reducer
