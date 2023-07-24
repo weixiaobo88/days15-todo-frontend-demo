39af5374d83785a2c33b3cee2f84886de7d6a717
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-07-24 16:51:46 +0800

    fix, use task id as todo-item key value

--- src/features/todo/components/TodoGroup.js
+++ src/features/todo/components/TodoGroup.js
@@ -5,8 +5,8 @@ export default function TodoGroup() {
     const todoTasks = useSelector(state => state.todo.tasks)
     return (
         <div className='todo-group'>
-            {todoTasks.map(((todoTask, index) =>
-                <TodoItem key= {index} task={todoTask}></TodoItem>
+            {todoTasks.map(((todoTask) =>
+                <TodoItem key= {todoTask.id} task={todoTask}></TodoItem>
             ))}
         </div>
     );
