d93fab3fb023e301c0a5d155413791da2357c14a
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-09-01 14:31:47 +0800

    feat, add multiple env api baseurl

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
-    baseURL: 'https://days15-todo-backend-demo-qa.up.railway.app/'
-});
+    'development': {
+        baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
+    },
+    'qa': {
+        baseURL: 'https://days15-todo-backend-demo-qa.up.railway.app/'
+    }
+}[process.env.REACT_APP_ENV || 'qa']);
 export default api;
