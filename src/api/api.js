bdb23ed28a8826bcfe10ac50c48fbd8385d78030
Zhaoyu <weixiaobo88@gmail.com>
2023-09-01 15:57:06 +0800

    fix: fix the usage of base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,27 +1,11 @@
 import axios from "axios";
 
-const api = axios.create(getBaseURLByEnv());
+const LOCAL_BASE_URL = "http://localhost:8080/";
+const baseURLByEnv = process.env.REACT_APP_BASE_URL || LOCAL_BASE_URL;
 
-function getBaseURLByEnv() {
-  const DEV_URL = "https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/";
-  const QA_URL = "https://days15-todo-backend-demo-qa.up.railway.app/";
-  const PROD_URL =
-    "https://days15-todo-backend-demo-production.up.railway.app/";
+const api = axios.create({
+  baseURL: baseURLByEnv,
+});
 
-  const baseURL = {
-    development: {
-      baseURL: DEV_URL,
-    },
-    qa: {
-      baseURL: QA_URL,
-    },
-    prod: {
-      baseURL: PROD_URL,
-    },
-  }[process.env.REACT_APP_ENV || "development"];
-
-  if (!baseURL) return { baseURL: QA_URL };
-  return baseURL;
-}
 
 export default api;
--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,9 +1,10 @@
 export default function HelpPage() {
-  console.log("process.env.REACT_APP_ENV", process.env.REACT_APP_ENV);
-
+  console.log(process.env);
   return (
     <div>
       <h1> This is Help Page </h1>
+      <p>ENV: {process.env.REACT_APP_ENV}</p>
+      <p>BASE_URL: {process.env.REACT_APP_BASE_URL}</p>
     </div>
   );
 }
