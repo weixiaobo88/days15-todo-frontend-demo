55bd25fca52897676f8e38844403061d833aa1eb
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-09-01 13:59:16 +0800

    fix, update api url with qa backend endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
+    //baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
+    baseURL: 'https://days15-todo-backend-demo-qa.up.railway.app/'
 });
 export default api;
