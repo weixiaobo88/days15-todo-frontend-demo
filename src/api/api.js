dce1d030d2447fab2bd98d7f8025605d2f9c1e21
Zhaoyu <weixiaobo88@gmail.com>
2023-09-01 15:18:44 +0800

    chore: update envs

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 
-const api = axios.create({
-    'development': {
-        baseURL: 'https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/'
-    },
-    'qa': {
-        baseURL: 'https://days15-todo-backend-demo-qa.up.railway.app/'
-    }
-}[process.env.REACT_APP_ENV || 'development']);
+const api = axios.create(getBaseURLByEnv());
 export default api;
+
+function getBaseURLByEnv() {
+  const DEV_URL = "https://649138542f2c7ee6c2c7dd46.mockapi.io/api/v1/";
+  const QA_URL = "https://days15-todo-backend-demo-qa.up.railway.app/";
+  const PROD_URL =
+    "https://days15-todo-backend-demo-production.up.railway.app/todos";
+
+  const baseURL = {
+    development: {
+      baseURL: DEV_URL,
+    },
+    qa: {
+      baseURL: QA_URL,
+    },
+    prod: {
+      baseURL: PROD_URL,
+    },
+  }[process.env.REACT_APP_ENV || "development"];
+
+  if (!baseURL) return QA_URL;
+}
